5fad9fb50f878fc643fc297d3d6ea72775282bb9
Peter Anderson <Panderson@adaptavist.com>
2022-05-26 16:04:08 +0100

    working with enums

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
+var RoleID;
+(function (RoleID) {
+    RoleID[RoleID["USER"] = 0] = "USER";
+    RoleID[RoleID["OFFICER"] = 1] = "OFFICER";
+    RoleID[RoleID["ADMINID"] = 2] = "ADMINID";
+})(RoleID || (RoleID = {}));
+var RoleName;
+(function (RoleName) {
+    RoleName["Low"] = "User";
+    RoleName["Med"] = "officer";
+    RoleName["High"] = "admin";
+})(RoleName || (RoleName = {}));
 var person = {
     name: 'Peter',
     nickname: 'Pete',
-    age: 29
+    age: 29,
+    hobbies: ['skiing', 'dancing'],
+    role: [RoleID.USER, RoleName.Low]
 };
+var favouriteActivities; /* any type allows a mixed array. Ideally it's better to use typed variables. */
+favouriteActivities = ['Sports', 1];
+if (person.role[0] === 0) {
+    console.log('user ' + person.name + ' has security clearance ' + person.role[1]);
+}
+console.log(person.nickname);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var activities = _a[_i];
+    console.log(activities.toUpperCase());
+}
 /*
-const product {
+Object declaration.
+both acceptable, first better.
+
+const product: {
     id: string;
     price: number;
     tags: string[];
@@ -21,5 +48,10 @@ const product {
         description: 'A great carpet - almost brand-new!'
     }
 }
-*/
-console.log(person.nickname);
+
+const person: object = {
+    name = jim,
+    age = 40
+}
+
+*/ 
--- app.ts
+++ app.ts
@@ -1,12 +1,33 @@
-const person ={
+enum RoleID {USER = 0, OFFICER, ADMINID}
+enum RoleName {Low = 'User', Med = 'officer', High = 'admin'}
+
+const person: {
+    name: string;
+    nickname: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+}={
     name: 'Peter',
     nickname: 'Pete',
     age: 29,
-    hobbies: ['skiing', 'dancing']
+    hobbies: ['skiing', 'dancing'],
+    role: [RoleID.USER, RoleName.Low]
 };
 
-let favouriteActivities: any[];
+let favouriteActivities: any[]; /* any type allows a mixed array. Ideally it's better to use typed variables. */
 favouriteActivities = ['Sports', 1];
+
+if (person.role[0] === 0){
+    console.log('user ' + person.name + ' has security clearance ' + person.role[1]);
+}
+
+
+console.log(person.nickname);
+
+for (const activities of person.hobbies){
+    console.log(activities.toUpperCase());
+}
 
 /* 
 Object declaration.
@@ -36,5 +57,3 @@ const person: object = {
 }
 
 */
-
-console.log(person.nickname);
