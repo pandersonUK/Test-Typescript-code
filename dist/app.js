a82662b50059f83a05c621a1e82c25fd2dec7f25
Peter Anderson <Panderson@adaptavist.com>
2022-05-30 17:35:46 +0100

    Tuples, Arrays and More.

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,10 @@
 "use strict";
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
+const person = {
+    name: 'max',
+    age: 30
+};
+const copiedPerson = Object.assign({}, person);
+const add = (...numbers) => { return numbers.reduce((curResult, curValue) => { return curResult + curValue; }, 0); };
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
-const button = document.querySelector('button')!;
+// const button = document.querySelector('button')!;
+// const add = (a: number, b: number = 21) => a + b;
+// const printOutput: (a: number | string) => void = output => console.log(output);
 
-button.addEventListener('click', () => {
-    console.log('Clicked!');
+// button.addEventListener('click', () => {
+//     console.log('Clicked!');
+// }
+// )
+
+// printOutput(add(5));
+
+
+const person = {
+    name: 'max',
+    age: 30
 }
-)
+
+const copiedPerson = { ...person};
+
+const add = (...numbers: number[]) => { return numbers.reduce((curResult, curValue) => { return curResult + curValue; },0);}
+
+const addedNumbers = add (5,10,2,3.7);
+console.log(addedNumbers);
