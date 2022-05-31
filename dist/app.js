1b22619daade6ac728275580ca514854d66040b4
Peter Anderson <Panderson@adaptavist.com>
2022-05-31 10:00:13 +0100

    working with arrays advanced, structuring/destructuring

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,18 @@
 "use strict";
+const hobbies = ["Running", "Cooking", "Fishing", "Dancing", "Leaping"];
+const activeHobbies = ["Hiking"];
+activeHobbies.push(...hobbies);
+console.log(...hobbies);
+console.log(hobbies);
 const person = {
-    name: 'max',
+    name: 'Max',
     age: 30
 };
 const copiedPerson = Object.assign({}, person);
+console.log(copiedPerson);
 const add = (...numbers) => { return numbers.reduce((curResult, curValue) => { return curResult + curValue; }, 0); };
 const addedNumbers = add(5, 10, 2, 3.7);
 console.log(addedNumbers);
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobby1, hobby2, remainingHobbies, hobbies);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,29 @@
 // )
 
 // printOutput(add(5));
+const hobbies = ["Running", "Cooking", "Fishing", "Dancing", "Leaping"];
+const activeHobbies = ["Hiking"];
 
+activeHobbies.push(...hobbies);
+console.log(...hobbies);
+console.log(hobbies);
 
 const person = {
-    name: 'max',
-    age: 30
+    firstName: 'Maxi',
+    lastName: 'Pad',
+    age: 69
 }
 
 const copiedPerson = { ...person};
+console.log(copiedPerson);
 
 const add = (...numbers: number[]) => { return numbers.reduce((curResult, curValue) => { return curResult + curValue; },0);}
 
 const addedNumbers = add (5,10,2,3.7);
 console.log(addedNumbers);
+
+//Restructure Hobbies array into Hobby1, Hobby2 and remainingHobbies
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobby1, hobby2, remainingHobbies, hobbies);
+//Javascript aliasing code, swapping firstName to userName
+const { firstName: userName, lastName, age} = person;
